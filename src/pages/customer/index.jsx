ae02f915420914033e675dfc12025b4fe0965eda
Phong Nguyen <phchmessibar326@gmail.com>
2024-06-26 06:45:53 +0700

    table for user & order

--- src/pages/customer/index.jsx
+++ src/pages/customer/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import Loading from '@components/loading'
 import useFetch from '@hooks/useFetch'
+import {
+  Avatar,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow
+} from '@mui/material'
 import { getUserList } from '@services/user-service'
 
 import styles from './index.module.scss'
@@ -10,13 +21,65 @@ const CustomerPage = () => {
     queryFunction: getUserList
   })
 
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(5)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
   if (isLoading) return <Loading />
   return (
     <div className={styles.pageContainer}>
       <div className={styles.pageHeader}>
         <h3 className={styles.title}>Khách hàng</h3>
       </div>
-      <div>{JSON.stringify(response)}</div>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer>
+          <Table stickyHeader aria-label='sticky table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Avatar</TableCell>
+                <TableCell>Full Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Phone Number</TableCell>
+                <TableCell>Username</TableCell>
+                <TableCell>Role</TableCell>
+                <TableCell>Gender</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {response.users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((user) => (
+                <TableRow key={user._id}>
+                  <TableCell>
+                    <Avatar alt={user.fullName} src={user.avatar} />
+                  </TableCell>
+                  <TableCell>{user.fullName}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                  <TableCell>{user.phoneNumber}</TableCell>
+                  <TableCell>{user.username}</TableCell>
+                  <TableCell>{user.role}</TableCell>
+                  <TableCell>{user.gender}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component='div'
+          count={response.pagination.totalUsers}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
     </div>
   )
 }
--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import Loading from '@components/loading'
 import useFetch from '@hooks/useFetch'
+import {
+  Avatar,
+  Chip,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow
+} from '@mui/material'
 import { getOrderList } from '@services/order-service'
-
+const statusColors = {
+  paid: 'green',
+  pending: 'orange',
+  canceled: 'red',
+  delivered: 'blue'
+}
 import styles from './index.module.scss'
 
 const OrderPage = () => {
@@ -10,13 +27,84 @@ const OrderPage = () => {
     queryFunction: getOrderList
   })
 
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(5)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
+  const calculateOrderValue = (items) => {
+    return items.reduce((total, item) => {
+      const itemValue = item.price * item.quantity - item.discount
+      return total + itemValue
+    }, 0)
+  }
+
   if (isLoading) return <Loading />
   return (
     <div className={styles.pageContainer}>
       <div className={styles.pageHeader}>
         <h3 className={styles.title}>Đơn hàng</h3>
       </div>
-      <div>{JSON.stringify(response)}</div>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer>
+          <Table stickyHeader aria-label='sticky table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Thứ tự</TableCell>
+                <TableCell>Khách hàng</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>SDT</TableCell>
+                <TableCell>Địa chỉ</TableCell>
+                <TableCell>Trạng thái</TableCell>
+                <TableCell>Thanh toán</TableCell>
+                <TableCell>Ngày đặt</TableCell>
+                <TableCell>Chi phí</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {response.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((order, index) => (
+                <TableRow key={order._id}>
+                  <TableCell>{page * rowsPerPage + index + 1}</TableCell>
+                  <TableCell>{order.customerName}</TableCell>
+                  <TableCell>{order.customerEmail}</TableCell>
+                  <TableCell>{order.customerPhone}</TableCell>
+                  <TableCell>
+                    {order.delivery.detailAddress}, {order.delivery.wardName}, {order.delivery.districtName},{' '}
+                    {order.delivery.provinceName}
+                  </TableCell>
+                  <TableCell>
+                    <Chip
+                      label={order.status}
+                      style={{ backgroundColor: statusColors[order.status], color: 'white' }}
+                    />
+                  </TableCell>
+                  <TableCell>{order.paymentMethod}</TableCell>
+                  <TableCell>{new Date(order.orderDate).toLocaleDateString()}</TableCell>
+                  <TableCell>
+                    {calculateOrderValue(order.items).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component='div'
+          count={response.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
     </div>
   )
 }
